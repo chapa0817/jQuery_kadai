decfb73ce6f8825f299b52c267c3f3fe7080b7b1
chapa0817 <145003120+chapa0817@users.noreply.github.com>
2023-11-11 15:08:51 -0500

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,16 @@
+   $('#change-color').on('click', function(){
+    $('#target').css('color', 'gold');
+  });
+
+  $('#change-text').on('click', function(){
+    $('#target').text('Hey Samurai boy');
+  });
+
+  $('#fade-out').on('click', function(){
+    $('#target').fadeOut()
+    });
+
+    $('#fade-in').on('click', function(){
+      $('#target').fadeIn()
+      });
+    
